d702ed09743e8d608be897d32f26de36a9aa4c18
Asim-AAK <60318842+Asim-AAK@users.noreply.github.com>
2023-12-02 00:30:59 +0500

    random generation of platform added

--- Game/Platform.cpp
+++ Game/Platform.cpp
@@ -1,4 +1,6 @@
 #include "Platform.hpp"
+#include <cstdlib>
+#include <ctime>
 
 std::vector<Platform> platformVector;
 
@@ -7,10 +9,10 @@ void createPlatform(int x, int y, bool type) {
     int speed;
 
     if (type) {
-        platformRect = {1142, 440, 130, 388};
+        platformRect = {1142, 473, 130, 355};
         speed = 10;  // Speed for the bottom platform
     } else {
-        platformRect = {979, 439, 130, 388};
+        platformRect = {979, 439, 130, 351};
         speed = 10;  // Speed for the top platform
     }
 
@@ -19,12 +21,31 @@ void createPlatform(int x, int y, bool type) {
 }
 
 void updatePlatforms() {
+    static Uint32 startTime = SDL_GetTicks();  // Static to keep track of the start time
+    Uint32 currentTime = SDL_GetTicks();
+    Uint32 elapsedTime = currentTime - startTime;
+
     for (auto& platform : platformVector) {
         if (platform.active) {
             platform.moverRect.x -= platform.speed;
-            // additional logic to handle when platforms go off-screen
-            // and whether to reset or remove them from the vector will decide later
+
+            // Check if the platform has reached the left side of the screen
+            if (platform.moverRect.x + platform.moverRect.w < 0) {
+                platform.active = false;  // Deactivate the platform
+
+                // Randomly generate a new platform (top or bottom)
+                bool isTopPlatform = rand() % 2 == 0;
+                int newY = isTopPlatform ? 0 : 500;  // Adjust the y-coordinate as needed
+
+                createPlatform(1280, newY, isTopPlatform);
+            }
         }
+    }
+
+    // Check if 30 seconds have elapsed, reset the timer, and clear the platforms
+    if (elapsedTime >= 40000) {
+        startTime = currentTime;
+        clearPlatforms();
     }
 }
 
@@ -34,4 +55,8 @@ void drawPlatforms(SDL_Renderer* gRenderer, SDL_Texture* assets) {
             SDL_RenderCopy(gRenderer, assets, &platform.srcRect, &platform.moverRect);
         }
     }
+}
+
+void clearPlatforms() {
+    platformVector.clear();
 }
--- Game/Platform.hpp
+++ Game/Platform.hpp
@@ -13,3 +13,4 @@ extern std::vector<Platform> platformVector;
 void createPlatform(int x, int y, bool type);
 void updatePlatforms();
 void drawPlatforms(SDL_Renderer* gRenderer, SDL_Texture* assets);
+void clearPlatforms();
