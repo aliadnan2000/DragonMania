6e45718a7f8fe4b910e2254dec7f55e30dabb038
aliadnan2000 <60284195+aliadnan2000@users.noreply.github.com>
2023-12-02 16:52:34 +0500

    movement added

---
+++ GameFolder/game.hpp
@@ -0,0 +1,14 @@
+#include <SDL_mixer.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+#pragma once
+
+class Game{
+
+};
